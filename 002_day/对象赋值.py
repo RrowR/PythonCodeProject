f6d325f2e44dbf66529913d4acd8db56a50185ee
Memo <2415782224@qq.com>
2025-05-18 08:04:56 +0800

    002_update

---
+++ 002_day/对象赋值.py
@@ -0,0 +1,7 @@
+user_num = '21321321312'
+
+print("this num is :" + user_num)
+
+# print = '123'
+#
+# print(print)
---
+++ 002_day/获取字符串长度.py
@@ -0,0 +1,13 @@
+print(len('2312'))
+
+print(type("2131"))
+print(type(1))
+print(type(False))
+print(type(None))
+print(type(print()))
+
+print(1/0)
+
+s = 'abc'
+
+print(s[1])
