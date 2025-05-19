6eeb85c6827211fcfc9c323de09dfef4febf0558
Memo <2415782224@qq.com>
2025-05-19 17:37:33 +0800

    文件读写测试题.py

---
+++ 004_day/文件读写测试题.py
@@ -0,0 +1,10 @@
+with open("poem.txt","w",encoding="utf-8") as f:
+    f.write("我欲乘风归去,\n")
+    f.write("有空穹窿鱼鱼,\n")
+    f.write("高处不胜寒。\n")
+    f.close()
+
+
+with open("poem.txt","a",encoding="utf-8") as f:
+    f.write("起舞弄清影,\n")
+    f.write("何似在人间。\n")
