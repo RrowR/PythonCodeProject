de409f69e5c7db52bf2a844bd82d4f5b0f5a7074
Memo <2415782224@qq.com>
2026-01-28 17:55:10 +0800

    元组

--- 010_day/常用数据结构之元组.py
+++ 010_day/常用数据结构之元组.py
@@ -18,6 +18,84 @@ print(t2[::3])  # ('骆昊', '四川成都')      这里是有 :: 从0 开始，
 for x in t1:
     print(x)
 
+a = ()
+print(type(a))  # <class 'tuple'>
+b = ('hello')
+print(type(b))  # <class 'str'>
+c = (100)
+print(type(c))  # <class 'int'>
+d = ('hello', )
+print(type(d))  # <class 'tuple'>
+e = (100, )
+print(type(e))  # <class 'tuple'>
+
+# 打包操作
+a = 1, 10, 100
+print(type(a))  # <class 'tuple'>
+print(a)        # (1, 10, 100)
+# 解包操作
+i, j, k = a
+print(i, j, k)  # 1 10 100
+
+a = 1, 10, 100, 1000
+# i, j, k = a             # ValueError: too many values to unpack (expected 3)
+# i, j, k, l, m, n = a    # ValueError: not enough values to unpack (expected 6, got 4)
+
+a = 1, 10, 100, 1000
+i, j, *k = a
+print(i, j, k)        # 1 10 [100, 1000]
+i, *j, k = a
+print(i, j, k)        # 1 [10, 100] 1000
+*i, j, k = a
+print(i, j, k)        # [1, 10] 100 1000
+*i, j = a
+print(i, j)           # [1, 10, 100] 1000
+i, *j = a
+print(i, j)           # 1 [10, 100, 1000]
+i, j, k, *l = a
+print(i, j, k, l)     # 1 10 100 [1000]
+i, j, k, l, *m = a
+print(i, j, k, l, m)  # 1 10 100 1000 []
+
+a, b, *c = range(1, 10)
+print(a, b, c)
+a, b, c = [1, 10, 100]
+print(a, b, c)
+a, *b, c = 'hello'
+print(a, b, c)
+
+# 交换变量值
+a = 1
+b = 2
+a,b = b,a
+print(a,b)
+
+import timeit
+
+print('%.3f 秒' % timeit.timeit('[1, 2, 3, 4, 5, 6, 7, 8, 9]', number=10000000))
+print('%.3f 秒' % timeit.timeit('(1, 2, 3, 4, 5, 6, 7, 8, 9)', number=10000000))
+
+infos = ('骆昊', 43, True, '四川成都')
+# 将元组转换成列表
+print(list(infos))  # ['骆昊', 43, True, '四川成都']
+
+frts = ['apple', 'banana', 'orange']
+# 将列表转换成元组
+print(tuple(frts))  # ('apple', 'banana', 'orange')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
