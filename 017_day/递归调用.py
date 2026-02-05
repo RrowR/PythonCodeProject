bb55c8110b15a597ace05c3da47c24c36d539060
RrowR <2415782224@qq.com>
2026-02-05 11:09:28 +0800

    函数高级应用.py 取消装饰器.py 装饰器函数.py 递归调用.py

--- 017_day/递归调用.py
+++ 017_day/递归调用.py
@@ -1,3 +1,6 @@
+import time
+
+
 def fac(num):
     if num in (0, 1):
         return 1
@@ -6,12 +9,38 @@ def fac(num):
 
 print(fac(5))    # 120
 
+from functools import lru_cache
+
+@lru_cache()        # 飞一般的感觉
 def fib1(n):
     if n in (1, 2):
         return 1
     return fib1(n - 1) + fib1(n - 2)
 
-
+start = time.time()
 for i in range(1, 51):
     print(fib1(i))
+end = time.time()
+print(f"输出的结果为{end-start}")
+
+def fib2(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+for i in range(1, 51):
+    print(fib2(i))
+
+
+
+
+
+
+
+
+
+
+
+
 
