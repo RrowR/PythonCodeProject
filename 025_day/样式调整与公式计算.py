79960e7e16520248617927dd1282f3568c8979aa
Rrow <2415782224@qq.com>
2026-05-18 18:11:23 +0800

    样式调整与公式计算.py

---
+++ 025_day/样式调整与公式计算.py
@@ -0,0 +1,29 @@
+import openpyxl
+from openpyxl.styles import Font, Alignment, Border, Side
+
+# 对齐方式
+alignment = Alignment(horizontal='center', vertical='center')
+# 边框线条
+side = Side(color='ff7f50', style='mediumDashed')
+
+wb = openpyxl.load_workbook('考试成绩表.xlsx')
+sheet = wb.worksheets[0]
+
+# 调整行高和列宽
+sheet.row_dimensions[1].height = 30
+sheet.column_dimensions['E'].width = 120
+
+sheet['E1'] = '平均分'
+# 设置字体
+sheet.cell(1, 5).font = Font(size=18, bold=True, color='ff1493', name='华文楷体')
+# 设置对齐方式
+sheet.cell(1, 5).alignment = alignment
+# 设置单元格边框
+sheet.cell(1, 5).border = Border(left=side, top=side, right=side, bottom=side)
+for i in range(2, 7):
+    # 计算每个学生的平均分
+    sheet[f'E{i}'] = f'=average(B{i}:D{i})'
+    sheet.cell(i, 5).font = Font(size=12, color='4169e1', italic=True)
+    sheet.cell(i, 5).alignment = alignment
+
+wb.save('考试成绩表.xlsx')
