ae4d6fb1fa4638389947aab3e9e6ffa41d7771b3
Rrow <2415782224@qq.com>
2026-05-18 17:52:34 +0800

    写入excel文件

---
+++ 025_day/写Excel文件.py
@@ -0,0 +1,21 @@
+import random
+import openpyxl
+
+# 创建工作簿
+wb = openpyxl.Workbook()
+# 添加工作表
+sheet = wb.active
+sheet.title = '期末成绩'
+
+titles = ('姓名', '语文', '数学', '英语')
+for col_index, title in enumerate(titles):
+    sheet.cell(1, col_index + 1, title)
+
+names = {'关羽', '张飞', '赵云', '马超', '黄忠'}
+for row_index, name in enumerate(names):
+    sheet.cell(row_index + 2, 1, name)
+    for col_index in range(2, 5):
+        sheet.cell(row_index + 2, col_index, random.randrange(50, 101))
+
+# 保存工作簿
+wb.save('考试成绩表.xlsx')
