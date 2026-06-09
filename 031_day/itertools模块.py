db13ff6327a1622b62a924b8ce99e20ebc160854
Rrow <2415782224@qq.com>
2026-06-09 11:51:21 +0800

    itertools模块.py

--- 031_day/itertools模块.py
+++ 031_day/itertools模块.py
@@ -5,5 +5,14 @@ import itertools
 
 # 产生ABCD的全排列   排列组合
 result = list(itertools.permutations('ABCD'))
+# print(result)
 # 产生ABCDE的五选三组合
-itertools.combinations('ABCDE',3)
+b = list(itertools.combinations('ABCDE', 3))
+
+# 笛卡尔积
+# print(list(itertools.product('ABCD', repeat=2)))
+# print(list(itertools.product('ABCD', '123')))
+
+# 无线循环序列
+# print(list(itertools.cycle('ABC')))
+print(list(itertools.islice(itertools.cycle('ABC'), 20)))  # 取前20个数
